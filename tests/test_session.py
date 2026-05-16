62f95cd85b18b3d14d7af012b922f4caa105199e
Apricot S. <152096077+Apricot-S@users.noreply.github.com>
2026-05-17 02:49:05 +0900

    feat: Update User Agent (#62)
    * feat: Update User Agent
    
    * feat: Update User Agent in test

--- tests/test_session.py
+++ tests/test_session.py
@@ -7,5 +7,5 @@ from houou_logs.session import create_session
 
 def test_create_session() -> None:
     session = create_session()
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36 Edg/140.0.0.0"  # noqa: E501
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36 Edg/148.0.0.0"  # noqa: E501
     assert session.headers["User-Agent"] == user_agent
